c09962c336fbfdc2cfa20570afd3c1d2d6c7b5ea
Maxim-ilan Ganansia <m.gnansia@gmail.com>
2021-01-19 19:23:27 +0200

    Tou Bichvat in progress

--- src/Components/TouBichvat.jsx
+++ src/Components/TouBichvat.jsx
@@ -1,12 +1,74 @@
 import React from 'react';
 import '../App.css'
+import SederFruit from '../Images/SederFruits.png'
 
 
 class TouBichvat extends React.Component {
     render() {
         return (
             <div className="div-fete-touBichvat">
-                <h1 className="titre-fete-touBichvat">Tou Bichvat<i> 15 Chvat</i> </h1>
+                <div className="card-touBichvat-one">
+                    <div className="div-résumé-touBichvat">
+                        <h1 className="titre-resumé-touBichvat">Tou Bichvat<i> 15 Chvat</i><br />c'est quoi ?</h1>
+                        <p className="texte-fete-touBichvat"><b>-</b> Le 15ème jour de Chevat est le Nouvel An des Arbres, connu sous le nom de Tou BiChevat.<br /><br />
+                            <b>-</b> D’après la loi biblique, il existe un cycle agricole de sept ans qui s’achève par l’année sabbatique, <b>la Chemitah</b>.<br /><br />
+                            <b>-</b> Lorsque le Saint Temple se tenait à Jérusalem, la première, la seconde, la quatrième et la cinquième année de ce cycle, les fermiers devaient prélever une seconde dîme de leur récolte et la consommer à Jérusalem. Cette dîme s’appelle <b>Maasser Chéni</b>, la Seconde Dîme, car elle s’ajoute aux deux pour cent qui doivent être données au Cohen et aux dix pour cent qui sont donnés au Lévite. Les troisième et sixième années du cycle, la seconde dîme est donnée aux pauvres qui pouvaient la consommer où bon leur semble.<br />
+                            <b>-</b>L’année sabbatique, aucune dîme n’est prélevée. Tous les produits de la terre qui poussent cette année-là sont sans propriétaire et quiconque peut les prendre.<br /><br />
+                            <b>-</b>Il était donc primordial de déterminer le moment où la nouvelle année débute pour les produits agricoles. Nos Sages ont établi qu’un fruit dont la floraison s’est faite avant le 15 Chevat appartient à la récolte de l’année précédente.<br />
+                            Si la floraison s’est faite après, c’est un fruit de la « nouvelle année ». <br />
+                            Les céréales et les légumes ont le même Nouvel An que nous autres, humains, le 1er Tichri. <br /><br />
+                            <i>Pourquoi cette date ?</i><br />
+                            Dans l’espace méditerranéen, la saison des pluies commence à la fête de Souccot. Il faut ensuite environ quatre mois <i>(depuis Souccot, le 15 Tichri, jusqu’au 15 Chevat)</i> pour que les pluies de la nouvelle année saturent le sol et les arbres et produisent des fruits. <br />
+                            Tous les fruits ayant fleuri auparavant sont donc un produit des pluies de l’année précédente et leur dîme est prélevée avec l’ensemble des récoltes de cette année.<br />
+                            Bien que ce jour soit un <b>Roch Hachana pour les arbres</b>, nous lui accordons beaucoup d’importance parce que <b>« L’homme est comparé à un arbre des champs »</b> <i>(Deutéronome 20,19)</i>.<br />
+                            En cultivant des racines profondes – notre foi en D.ieu et notre engagement dans Ses voies –, nous produisons de nombreux fruits : <b>Torah et Mitsvot</b>.
+                            </p>
+                    </div>
+                </div>
+                <div className="div-coutume">
+                    <iframe className="video-explain-touBichvat" width="560" height="315" src="https://www.youtube.com/embed/Lweglmm9xUc" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div className="coutume">
+                        <h1 className="titre-coutume">Coutumes</h1>
+                        <p className="texte-coutume">En ce jour il est de coutume de consommer les fruits avec lesquels la Torah fait l’éloge de la Terre Sainte (Deutéronome 8, 8) : <br/>
+                        <b>- Olives</b>, <br/> 
+                        <b>- Dattes</b>, <br/>
+                        <b>- Raisins</b>,<br/> 
+                        <b>- Figues</b>, <br/>
+                        <b>- Grenades</b>. <br/>
+                        Si vous goûtez à l’un de ces fruits pour la première fois cette saison, n’oubliez pas de réciter la bénédiction de <b>Chéhé’heyanou</b>. <i>Dans cette bénédiction, récitée en de joyeuses occasions, nous remercions D.ieu de nous avoir « accordé vie, soutenus et permis d'atteindre ce moment. »</i><br/>
+                        Cette bénédiction est dite avant celle de <b>« Ha’ets »</b> que l’on dit avant de consommer le fruit.<br/>
+                        De par la nature festive de ce jour, nous omettons les <b>Ta’hanoun</b> <i>(demande de pardon de nos fautes et confessions)</i> des prières.</p>
+                    </div>
+                </div>
+                <div className="segoulot-fruits">
+                    <div className="div-segoulot">
+                        <h1 className="titre-coutume">Segoulots</h1>
+                        <p className="texte-segoulot">Le rituel de Tou Bichevat n’est pas une obligation halakhique mais une <b>coutume</b> contenant de merveilleuses ségoulot <i>(phénomènes métaphysiques tirés de la Torah)</i> apportant abondance et bénédictions.<br/>
+                        Chaque espèce possède une <b>“ségoula” particulière</b>. Il est suffisant de regarder l’espèce et d’avoir l’intention adéquate en la mangeant pour profiter des bontés qu’elle peut nous apporter.<br/><br/>
+                        <h1 className="titre-segoula">Sagesse et Parnassa</h1>
+                        Le <b>blé</b> (gâteaux, pain) possède une ségoula pour <b>la lucidité, la sagesse et la réussite dans les études</b>. <br/>
+                        Demander aussi la parnassa (subsistance) dans l’abondance.<br/><br/>
+                        <h1 className="titre-segoula">Chalom Bayit</h1>
+                        <b>La bière et l’orge perlé</b> possèdent une ségoula pour le <b>chalom bayit</b>.<br/>
+                        Une femme sota (soupçonnée d’adultère, qui se voit être disculpée) devait amener un sacrifice d’orge, se purifier pour son mari, et méritait ainsi d’avoir un garçon.<br/><br/>
+                        <h1 className="titre-segoula">Mariage</h1>
+                        <b>Le vin, le jus de raisin, les raisins secs, les feuilles de vigne farcies</b> possèdent une ségoula pour les <b>zivougim</b><i>(trouver son conjoint)</i>.<br/>
+                        Ce n’est pas sans raison que lors d’un mariage nous chantons « Les raisins de la vigne s'assemblent aux raisins de la vigne ».<br/>
+                        Le raisin est aussi la <b>fécondité</b>, « Ta femme sera comme une vigne féconde ».<br/><br/>
+                        <h1 className="titre-segoula">Bouche pure</h1>
+                       <b>La grenade</b> a la même couleur que les lèvres, comme il est dit « Tes lèvres sont comme un fil d’écarlate ». Sa ségoula est dans la <b>protection de la bouche du Lachon hara</b> <i>(médisance)</i> et des <b>mauvaises paroles</b>.<br/><br/>
+                       <h1 className="titre-segoula">Enfants Tsadikim</h1>
+                        <b>Les olives et son huile</b> sont une ségoula pour <b>avoir des enfants tsadikim</b>.<br/>
+                        De la même manière que les feuilles des olives ne tombent pas, ainsi nous aurons le mérite que nos enfants ne se dirigeront vers aucune mauvaise direction.<br/><br/>
+                       <h1 className="titre-segoula">Santé</h1>
+                       <b>La datte</b> possède une merveilleuse ségoula pour <b>la grandeur et la réussite</b>.<br/>
+                        comme la datte est le cœur du ciel, ainsi nous demandons « aide-nous à nous élever comme la datte ».<br/>
+                        La datte est aussi une ségoula pour la <b>guérison et la bonne santé</b>.</p>
+                    </div>
+                    <div className="div-seder-fruits">
+                        <img className="seder-fruits" src={SederFruit} alt=""/>
+                    </div>
+                </div>
             </div>
         )
     }
